85a114e631b60a4a492566705a174c993fbd21a6
dali546 <35352237+dali546@users.noreply.github.com>
2019-06-12 21:07:03 +0100

    Redux Init
    Redux Store and basic Reducer and Action test created. Further functionality will come soon...

---
+++ lib/redux/middleware/custom_logging_middleware.dart
@@ -0,0 +1,22 @@
+import 'package:logging/logging.dart';
+import 'package:redux/src/store.dart';
+import 'package:redux_logging/redux_logging.dart';
+
+final Logger logger = new Logger("Redux-Log");
+
+class CustomLoggingMiddleware implements LoggingMiddleware {
+
+  @override
+  void call(Store store, action, NextDispatcher next) {
+    super(store,action,next);
+  }
+
+  @override
+  MessageFormatter get formatter => null;
+
+  @override
+  Level get level => Level.INFO;
+
+  @override
+  Logger get logger => logger;
+}
--- lib/redux/reducers/app_reducer.dart
+++ lib/redux/reducers/app_reducer.dart
@@ -1,5 +1,12 @@
+import 'package:community/redux/actions/ui_actions.dart';
 import 'package:community/redux/app_state.dart';
+import 'package:redux/redux.dart';
 
-AppState appReducer(AppState state, action) {
-  return AppState(appName: "Community - Reduced");
+final appReducer = combineReducers<AppState>([
+  TypedReducer<AppState,ChangeApplicationNameAction>(_changeApplicationNameReducer)
+]);
+
+
+AppState _changeApplicationNameReducer(AppState state, ChangeApplicationNameAction action) {
+  return state.copyWith(appName: action.appName);
 }
