85219dbfa4120448913c47e182c87b14ed204b27
dali546 <35352237+dali546@users.noreply.github.com>
2019-07-02 20:07:42 +0100

    Removed Redux Persistor (#15)
    Removed Redux Persistor (commented out) because of dependency issues

--- lib/redux/init.dart
+++ lib/redux/init.dart
@@ -2,26 +2,25 @@ import 'package:redux/redux.dart';
 import 'package:redux_logging/redux_logging.dart';
 import 'package:redux_persist/redux_persist.dart';
 import 'package:redux_persist_flutter/redux_persist_flutter.dart';
-
 import 'redux.dart';
 
 // Redux Initialisation called by main();
 Future<Store<AppState>> setupRedux() async {
   // Create Persistor - Stores Redux for next app startup, using JSON
-  final persistor = Persistor<AppState>(
-    storage: FlutterStorage(),
-    serializer: JsonSerializer<AppState>(AppState.fromJson),
-  );
+
+//  final persistor = Persistor<AppState>(
+//    storage: FlutterStorage(),
+//    serializer: JsonSerializer<AppState>(AppState.fromJson),
+//  );
 
   // Load initial state - Load existing state -- calls Appstate.fromJson method
-  final initialState = await persistor.load();
-
+  final initialState = AppState.initial();
   // Create Store - Holds global configuration of the app.
   final store = Store<AppState>(
     appReducer,
     initialState: initialState,
     middleware: [
-      persistor.createMiddleware(), // calls toJson to save config -- i think
+      // persistor.createMiddleware(), // calls toJson to save config -- i think
       new LoggingMiddleware.printer(formatter: customReduxLogFormatter)
     ],
   );
