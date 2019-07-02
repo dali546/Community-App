2acf6cb89347f98a667071f9eeb69afbf6c1db93
dali546 <35352237+dali546@users.noreply.github.com>
2019-07-02 20:07:42 +0100

    Removed Redux Persistor (#15)
    Removed Redux Persistor (commented out) because of dependency issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:community/view//appComponent.dart';
-import 'package:community/redux/app_state.dart';
-import 'package:community/redux/init.dart';
+import 'package:community/vendor/globals.dart';
 import 'package:community/view//app_component.dart';
 import 'package:flutter/material.dart';
-import 'package:redux/redux.dart';
 
 void main() async {
-  Store<AppState> store = await setupRedux();
-
+  await initialiseGlobals();
   runApp(AppComponent(store: store));
 }
